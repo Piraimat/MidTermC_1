12f7028811ad357db4f553708fb2b812db23f5ca
Piraino Matteo <matteopiraino05@gmail.com>
2022-06-21 13:10:16 -0400

    Merge origin/master
    Conflicts:
    	.gitignore
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

---
+++ src/MatteoPiraino/java.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package MatteoPiraino;
+
+/**
+ *
+ * Fundamentals of software design and development
+ *
+ * @author Matteo Piraino
+ */
+//This is the Java file created for the Branch of The Second part of 
+//the software development mid term
+public class java {
+
+}
